c5851f02b5e4cb53c239d883d43181278a6bae49
João Martins <joao@sousamartins.com>
2020-01-01 14:32:19 +0000

    Addressing PR comments. Improving logging.

--- timelapse.py
+++ timelapse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import logging
 import datetime
 import json
 import os
@@ -10,13 +11,19 @@ import textwrap
 import time
 import traceback
 import urllib3
+import shutil
+import subprocess
 from requests.auth import HTTPBasicAuth
 
 urllib3.disable_warnings()
 
-
-def log_print(*msg, file=sys.stdout):
-    print(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), *msg, file=file)
+# create logger
+logger = logging.getLogger('timelapse')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(ch)
 
 
 class SimpleLineProtocol:
@@ -59,20 +66,18 @@ def layer_changed(timelapse_folder, webcam_url, webcam_http_auth, webcam_https_v
         with open(pic, 'wb') as f:
             for chunk in r:
                 f.write(chunk)
-        log_print("Picture taken!", pic)
+        logger.info("Picture taken! " + pic)
     else:
-        log_print('Failed to get timelapse snapshot.', file=sys.stderr)
+        logger.warning('Failed to get timelapse snapshot.')
 
 def create_video(timelapse_path, current_log_print, snapshots_path, keep_snapshots):
     video_file = os.path.abspath(os.path.join(timelapse_path, current_log_print + ".mp4"))
-    snapshots_files = snapshots_path + os.path.sep + "*.jpg"
-    cmd = "ffmpeg -r 20 -y -pattern_type glob -i '" + snapshots_files + "' -vcodec libx264 " + video_file
-    os.system(cmd)
-    log_print(cmd)
+    snapshots_files = os.path.join(snapshots_path, "*.jpg")
+    subprocess.run(["ffmpeg", "-r 20", "-y", "-pattern_type glob", "-i '" + snapshots_files + "'", "-vcodec libx264", video_file])
     if not keep_snapshots:
         shutil.rmtree(timelapse_folder)
-        log_print("Snapshot files deleted")
-    log_print("Video created: " + video_file)
+        logger.info("Snapshot files deleted")
+    logger.info("Video created: " + video_file)
 
 
 def firmware_monitor(timelapse_folder, duet_host, webcam_url, webcam_http_auth, webcam_https_verify, run_ffmpeg, keep_snapshots):
@@ -80,11 +85,11 @@ def firmware_monitor(timelapse_folder, duet_host, webcam_url, webcam_http_auth, 
 
     while True:
         try:
-            log_print("Connecting to {}...".format(duet_host))
+            logger.info("Connecting to {}...".format(duet_host))
             sock = socket.create_connection((duet_host, 23), timeout=10)
             time.sleep(4.5)  # RepRapFirmware uses a 4-second ignore period after connecting
             conn = SimpleLineProtocol(sock)
-            log_print("Connection established.")
+            logger.info("Connection established.")
 
             timelapse_path = None
             snapshots_path = None
@@ -99,21 +104,21 @@ def firmware_monitor(timelapse_folder, duet_host, webcam_url, webcam_http_auth, 
                     # a print is running, but we don't know the filename yet
                     conn.write('M36')
                     json_data, raw_lines = conn.read_json_line()
-                    log_print("Print started:", json_data)
+                    logger.info("Print started:", json_data)
                     gcode_filename = os.path.basename(json_data['fileName'])
-                    current_log_print = "{}-{}".format(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M"),
+                    current_log_print = "{}-{}".format(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M"),
                                                        os.path.splitext(gcode_filename)[0])
                     timelapse_path = os.path.expanduser(timelapse_folder)
                     snapshots_path = os.path.abspath(os.path.join(timelapse_path, current_log_print))
                     os.makedirs(snapshots_path, exist_ok=True)
-                    log_print("New timelapse folder created: {}{}".format(snapshots_path, os.path.sep))
-                    log_print("Waiting for layer changes...")
+                    logger.info("New timelapse folder created: {}{}".format(snapshots_path, os.path.sep))
+                    logger.info("Waiting for layer changes...")
                 if status == 'I' and snapshots_path:
                     if run_ffmpeg: 
-                    	create_video(timelapse_path, current_log_print, snapshots_path, keep_snapshots)
+                        create_video(timelapse_path, current_log_print, snapshots_path, keep_snapshots)
                     # a previous print finished and we need to reset and wait for a new print to start
                     snapshots_path = None
-                    log_print("Print finished.")
+                    logger.info("Print finished.")
 
                 if snapshots_path:
                     for line in raw_lines:
@@ -122,9 +127,8 @@ def firmware_monitor(timelapse_folder, duet_host, webcam_url, webcam_http_auth, 
 
                 time.sleep(1)
         except Exception as e:
-            log_print('ERROR', e, file=sys.stderr)
-            traceback.print_exc()
-        log_print("Sleeping for a bit...", file=sys.stderr)
+            logger.exception("ERROR")
+        logger.info("Sleeping for a bit...")
         time.sleep(15)
 
 
@@ -138,22 +142,22 @@ if __name__ == "__main__":
             The filename of the timelapse video will have the starting print timestamp and g-code filename.
             You can choose to disable the video generator and keep the individual snapshot files instead.
             A new subfolder will be created with a timestamp and g-code filename for every new log_print.
-            
-            This script connects via Telnet to your log_printer, make sure to enable it in your config.g:
-                M586 P2 S1 ; enable Telnet
-            
+
+            This script connects via  to your log_printer, make sure to enable it in your config.g:
+                M586 P2 S1 ; enable 
+
             You need to inject the following G-Code before a new layer starts:
                 M400 ; wait for all movement to complete
                 M118 P4 S"LAYER CHANGE" ; take a picture
                 G4 P500 ; wait a bit
-            
+
             If you are using Cura, you can use the TimelapseLayerChange.py script with the Cura Post-Processing plugin.
             If you are using Simplify3D, you can enter the above commands in the "Layer Change Script" section of your process.
             Slicer-generated z-hops might cause erronously taken pictures, use firmware-retraction with z-hop instead.
-            
+
             You can disable the video and render a timelapse movie manually with the program ffmpeg:
                 $ ffmpeg -r 20 -y -pattern_type glob -i '*.jpg' -c:v libx264 output.mp4
-            
+
             Usage: ./timelapse.py <folder> <duet_host> <webcam_url> [<auth>] [--no-verify] [--no-ffmpeg] [--keep-snapshots]
             
                 folder            - folder where all videos and snapshots will be collected, e.g., ~/timelapses
